85f886fc92d570e91b49446d77893ca4c4b0d8ea
phemy173 <duyile.valentine@gmail.com>
2023-08-05 19:52:02 +0100

    Scroll to top Updated

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -30,10 +30,7 @@ const Footer = () => {
                     <div class="w-100 d-none d-block mt-5"></div>
                     <hr className='border border-white border-2 d-md-none d-block'/>
                     <div className='col-6 col-md-2 menu text-white'>
-
-                        <h4>
-                            <a href="/tour" className='nav-link'>Our Team</a></h4>
-            
+                        <h4><a href="/tour" className='nav-link'>Our Team</a></h4>
                             <p><a href=" " className='nav-link'>Contact Us</a></p>
                             <p><a href="/review" className='nav-link'>Leave a review</a></p>
                     </div>
@@ -51,16 +48,14 @@ const Footer = () => {
                 <div className="container mt-3">
                     <div className="col-6 text-white">
                         <h5 className='fw-normal'>For questioning and <br className='d-none d-md-block'/>booking, please contact:</h5>
-                        <h5 className='fw-normal'>E-mail : info@adventour.com</h5>
-                        
+                        <h5 className='fw-normal'>E-mail : adventourekiti@gmail.com</h5>
                     </div>
                 </div>
             </div>
             <div>
                 <hr class="border border-white border-2 "></hr>
                 <h6 className='rightreserved'>&copy; Adventour, 2023. All Rights Reserved</h6>
-            </div>
-                
+            </div>                
         </div>
     </section>
   )
